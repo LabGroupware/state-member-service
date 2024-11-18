b9c971ca1218e8467504e86e9caf92c4ec550d5c
omniarchy <k.hayashi@cresplanex.com>
2024-11-19 03:03:32 +0900

    fix

--- src/main/resources/db/migration/ddl/V20241102_212716__create_user_profiles_table.sql
+++ src/main/resources/db/migration/ddl/V20241102_212716__create_user_profiles_table.sql
@@ -16,9 +16,9 @@ CREATE TABLE user_profiles (
     birthdate DATE,
     zoneinfo VARCHAR(50),
     locale VARCHAR(10),
-    created_at date NOT NULL,
+    created_at TIMESTAMP NOT NULL,
     created_by varchar(50) NOT NULL,
-    updated_at date DEFAULT NULL,
+    updated_at TIMESTAMP DEFAULT NULL,
     updated_by varchar(50) DEFAULT NULL
 );
 
